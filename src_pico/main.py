dc935b88dee0071f9488c85dae5a70e5178ebe9c
Kevin <kevin.bruno.work@gmail.com>
2026-09-01 11:15:25 +0200

    repo structure

---
+++ src_pico/main.py
@@ -0,0 +1,10 @@
+from wifi import connect_wifi
+from machine import Pin
+
+status_led = Pin(15, 1)
+
+
+if connect_wifi():
+    status_led.value(1)
+else:
+    status_led.value(0)
---
+++ src_pico/wifi.py
@@ -0,0 +1,33 @@
+import json 
+import network
+import rp2
+import time
+import requests
+
+rp2.country("SE")
+
+with open("wifi_credentials.json") as file:
+    credentials = json.load(file)
+
+def connect_wifi(waiting_time = 10):
+    # pico becomes a client to connect to WIFI
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True) # powers on radio WIFI on pico W/2W
+    wlan.connect(credentials.get("SSID"), credentials.get("PASSWORD"))
+
+    while waiting_time > 0:
+        if wlan.isconnected():
+            print("congraz u connected")
+            break
+
+        waiting_time -= 1
+        print("Try to connect to wifi, waitu a little bit")
+        time.sleep(2)
+
+    return wlan.isconnected()
+
+url = "https://open-meteo.com/en/docs?latitude=59.3454275&longitude=18.0225"
+
+response = requests.get(url).json()
+
+print(response)
