8ecb081a4a3e8aed5cfe182e2d4422670c8c5f7f
Kevin <kevin.bruno.work@gmail.com>
2026-09-01 11:41:45 +0200

    testing wifi and dht sensor

--- src_pico/main.py
+++ src_pico/main.py
@@ -1,10 +1,20 @@
+import time
 from wifi import connect_wifi
 from machine import Pin
+from dht import DHT11
 
-status_led = Pin(15, 1)
+time.sleep(0.1)
 
+led = Pin(15, Pin.OUT)
+dht_sensor = DHT11(Pin(16))
 
 if connect_wifi():
-    status_led.value(1)
-else:
-    status_led.value(0)
+    led.value(1)
+
+while True:
+    dht_sensor.measure()
+    temp = dht_sensor.temperature()
+    humidity = dht_sensor.humidity()
+
+    data = {"temperature": temp, "humidity": humidity}
+    print(data)
